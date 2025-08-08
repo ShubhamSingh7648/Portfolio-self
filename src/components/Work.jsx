15f4dffc21e47270cb24f4cb95afe1d7ab080729
shubham7648 <shubhams7648@gmail.com>
2025-08-08 11:39:46 +0530

    minor updates

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,41 +2,36 @@ import React from 'react'
 import ProjectCard from './ProjectCard';
 const works = [
     {
-      imgSrc: '/images/project-1.jpg',
-      title: 'Full stack music app',
+      imgSrc: '/images/project-1.png',
+      title: 'foodies',
       tags: ['API', 'MVC', 'Development'],
       projectLink: 'https://musify-5al0.onrender.com/'
     },
     {
-      imgSrc: '/images/project-2.jpg',
-      title: 'Free stock photo app',
+      imgSrc: '/images/project-2.png',
+      title: 'Movie search',
       tags: ['API', 'SPA'],
       projectLink: 'https://pixstock-official.vercel.app/'
     },
     {
-      imgSrc: '/images/project-3.jpg',
-      title: 'Recipe app',
+      imgSrc: '/images/project-3.png',
+      title: 'King Of Diamond',
       tags: ['Development', 'API'],
       projectLink: ''
     },
     {
-      imgSrc: '/images/project-4.jpg',
-      title: 'Real state website',
+      imgSrc: '/images/project-4.png',
+      title: 'portfolio',
       tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee-org/wealthome'
+      projectLink: 'https://github.com/codewithsadee-org/https://portfolio-t21s.vercel.app/'
     },
     {
-      imgSrc: '/images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+      imgSrc: '/images/project-5.png',
+      title: 'Online hotel reservation',
+      tags: [ 'Development'],
+      projectLink: 'https://wonder-lust-beryl.vercel.app/listings'
     },
-    {
-      imgSrc: '/images/project-6.jpg',
-      title: 'vCard Personal portfolio',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
-    },
+
   ];
 
 const Work = () => {
